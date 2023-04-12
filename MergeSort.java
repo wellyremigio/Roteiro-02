ceb02f07856104f1dc8aec1b0a7f016fa9c676f1
Welly Remígio <85763844+wellyremigio@users.noreply.github.com>
2023-04-12 09:38:44 -0300

    OFICIAL

--- MergeSort.java
+++ MergeSort.java
@@ -1,46 +1,49 @@
-package ordenacao;
+package sorting.divideAndConquer;
 
-import java.util.Arrays;
+import sorting.AbstractSorting;
 
-public class MergeSort {
+/**
+ * Merge sort is based on the divide-and-conquer paradigm. The algorithm
+ * consists of recursively dividing the unsorted list in the middle, sorting
+ * each sublist, and then merging them into one single sorted list. Notice that
+ * if the list has length == 1, it is already sorted.
+ */
+public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
-	public static void main(String[] args) {
-		int[] vetor = {4, 6, 7, 3, 5, 1, 2, 8};
-		int[] vetorAux = new int[vetor.length];
-		
-		mergeSort(vetor, vetorAux, 0, vetor.length-1);
-		System.out.println(Arrays.toString(vetor));
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		T[] arrayAux = (T[]) new Comparable[array.length];
+		mergeSort(array, arrayAux, leftIndex, rightIndex);
 	}
-	
-	
-	public static void mergeSort(int[] vetor, int[] vetorAux, int inicio, int fim) {
+
+	private void mergeSort(T[] array, T[] arrayAux, int inicio, int fim) {
 		if(inicio < fim) {
-			int meio = (inicio + fim)/2;
-			mergeSort(vetor, vetorAux, inicio, meio);
-			mergeSort(vetor, vetorAux, meio+1, fim);
-			intercalar(vetor, vetorAux, inicio, meio, fim);
+			int meio = (inicio+fim)/2;
+			mergeSort(array, arrayAux, inicio, meio);
+			mergeSort(array, arrayAux, meio+1, fim);
+			intercalar(array, arrayAux, inicio, meio, fim);
 		}
+		
 	}
-	
-	public static void intercalar(int[] vetor, int[] vetorAux, int inicio, int meio, int fim) {
+
+	private void intercalar(T[] array, T[] arrayAux, int inicio, int meio, int fim) {
 		for(int k = inicio; k <= fim; k++) {
-			vetorAux[k] = vetor[k];
+			arrayAux[k] = array[k];
 		}
 		
 		int esquerda = inicio;
 		int direita = meio+1;
 		
 		for(int i = inicio; i <= fim; i++) {
-			if(esquerda > meio) { //Se o vetor da esquerda acabou
-				vetor[i] = vetorAux[direita++];
-			} else if(direita > fim) { //Se o vetor da direita acabou
-				vetor[i] = vetorAux[esquerda++];
-			} else if(vetorAux[esquerda] < vetorAux[direita]) {
-				vetor[i] = vetorAux[esquerda++];
+			if(esquerda > meio) {
+				array[i] = arrayAux[direita++]; 
+			} else if(direita > fim) {
+				array[i] = arrayAux[esquerda++];
+			} else if(arrayAux[esquerda].compareTo(arrayAux[direita]) < 0) {
+				array[i] = arrayAux[esquerda++];
 			} else {
-				vetor[i] = vetorAux[direita++];
+				array[i] = arrayAux[direita++];
 			}
 		}
 	}
-
 }
