be52b60b87f92d0b575c63608072cc8619f6d7e0
Welly Remígio <85763844+wellyremigio@users.noreply.github.com>
2023-04-14 11:10:36 -0300

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -1,83 +1,56 @@
-package ordenacao;
+package sorting.divideAndConquer;
 
-import java.util.Arrays;
+import sorting.AbstractSorting;
 
-public class MergeSort {
+/**
+ * Merge sort is based on the divide-and-conquer paradigm. The algorithm
+ * consists of recursively dividing the unsorted list in the middle, sorting
+ * each sublist, and then merging them into one single sorted list. Notice that
+ * if the list has length == 1, it is already sorted.
+ */
+public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
-	public static void main(String[] args) {
-		int[] vetor = {4, 6, 7, 3, 5, 1, 2, 8};
-		int[] vetorAux = new int[vetor.length];
-		
-		mergeSort(vetor, vetorAux, 0, vetor.length-1);
-		System.out.println(Arrays.toString(vetor));
-	}
-	
-	
-	public static void mergeSort(int[] vetor, int[] vetorAux, int inicio, int fim) {
-		if(inicio < fim) {
-			int meio = (inicio + fim)/2;
-			mergeSort(vetor, vetorAux, inicio, meio);
-			mergeSort(vetor, vetorAux, meio+1, fim);
-			intercalar(vetor, vetorAux, inicio, meio, fim);
-		}
-	}
-	
-	public static void intercalar(int[] vetor, int[] vetorAux, int inicio, int meio, int fim) {
-		for(int k = inicio; k <= fim; k++) {
-			vetorAux[k] = vetor[k];
-		}
-		
-		int esquerda = inicio;
-		int direita = meio+1;
-		
-		for(int i = inicio; i <= fim; i++) {
-			if(esquerda > meio) { //Se o vetor da esquerda acabou
-				vetor[i] = vetorAux[direita++];
-			} else if(direita > fim) { //Se o vetor da direita acabou
-				vetor[i] = vetorAux[esquerda++];
-			} else if(vetorAux[esquerda] < vetorAux[direita]) {
-				vetor[i] = vetorAux[esquerda++];
-			} else {
-				vetor[i] = vetorAux[direita++];
-			}
-		}
-	}
-	
+	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		T[] arrayAux = (T[]) new Comparable[array.length];
-		mergeSort(array, arrayAux, leftIndex, rightIndex);
-	}
-
-	private void mergeSort(T[] array, T[] arrayAux, int inicio, int fim) {
-		if(inicio < fim) {
-			int meio = (inicio+fim)/2;
-			mergeSort(array, arrayAux, inicio, meio);
-			mergeSort(array, arrayAux, meio+1, fim);
-			intercalar(array, arrayAux, inicio, meio, fim);
+		if(leftIndex < rightIndex) {
+			int middle = (leftIndex + rightIndex) / 2;
+			sort(array, leftIndex, middle);
+			sort(array, middle+1, rightIndex);
+			merge(array, leftIndex, middle, rightIndex);
 		}
-		
 	}
 
-	/*private void intercalar(T[] array, T[] arrayAux, int inicio, int meio, int fim) {
+	private void merge(T[] array, int inicio, int meio, int fim) {
+		T[] arrayAux = (T[]) new Comparable[array.length];
 		for(int k = inicio; k <= fim; k++) {
 			arrayAux[k] = array[k];
 		}
+	
+		int i = inicio;
+		int j = meio+1;
+		int k = inicio;
 		
-		int esquerda = inicio;
-		int direita = meio+1;
-		
-		for(int i = inicio; i <= fim; i++) {
-			if(esquerda > meio) {
-				array[i] = arrayAux[direita++]; 
-			} else if(direita > fim) {
-				array[i] = arrayAux[esquerda++];
-			} else if(arrayAux[esquerda].compareTo(arrayAux[direita]) < 0) {
-				array[i] = arrayAux[esquerda++];
-			} else {
-				array[i] = arrayAux[direita++];
+		while(i <= meio && j <= fim) {
+			if(arrayAux[i].compareTo(arrayAux[j]) < 0){
+				array[k] = arrayAux[i];
+				i++;
+			}else {
+				array[k] = arrayAux[j];
+				j++;
 			}
+			k++;
+			
+		}
+		
+		while (i <= meio) {
+			array[k] = arrayAux[i];
+			i++;
+			k++;
+		}
+		while(j <= meio) {
+			array[k] = arrayAux[j];
+			j++;
+			k++;
 		}
 	}
-	*/
-
 }
