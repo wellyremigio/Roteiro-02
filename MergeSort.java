e4fde7530f7a4f1d92469627ad4da9ad9b838b33
Welly Remígio <85763844+wellyremigio@users.noreply.github.com>
2023-04-14 10:40:28 -0300

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -1,16 +1,48 @@
-package sorting.divideAndConquer;
+package ordenacao;
 
-import sorting.AbstractSorting;
+import java.util.Arrays;
 
-/**
- * Merge sort is based on the divide-and-conquer paradigm. The algorithm
- * consists of recursively dividing the unsorted list in the middle, sorting
- * each sublist, and then merging them into one single sorted list. Notice that
- * if the list has length == 1, it is already sorted.
- */
-public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
+public class MergeSort {
 
-	@Override
+	public static void main(String[] args) {
+		int[] vetor = {4, 6, 7, 3, 5, 1, 2, 8};
+		int[] vetorAux = new int[vetor.length];
+		
+		mergeSort(vetor, vetorAux, 0, vetor.length-1);
+		System.out.println(Arrays.toString(vetor));
+	}
+	
+	
+	public static void mergeSort(int[] vetor, int[] vetorAux, int inicio, int fim) {
+		if(inicio < fim) {
+			int meio = (inicio + fim)/2;
+			mergeSort(vetor, vetorAux, inicio, meio);
+			mergeSort(vetor, vetorAux, meio+1, fim);
+			intercalar(vetor, vetorAux, inicio, meio, fim);
+		}
+	}
+	
+	public static void intercalar(int[] vetor, int[] vetorAux, int inicio, int meio, int fim) {
+		for(int k = inicio; k <= fim; k++) {
+			vetorAux[k] = vetor[k];
+		}
+		
+		int esquerda = inicio;
+		int direita = meio+1;
+		
+		for(int i = inicio; i <= fim; i++) {
+			if(esquerda > meio) { //Se o vetor da esquerda acabou
+				vetor[i] = vetorAux[direita++];
+			} else if(direita > fim) { //Se o vetor da direita acabou
+				vetor[i] = vetorAux[esquerda++];
+			} else if(vetorAux[esquerda] < vetorAux[direita]) {
+				vetor[i] = vetorAux[esquerda++];
+			} else {
+				vetor[i] = vetorAux[direita++];
+			}
+		}
+	}
+	
 	public void sort(T[] array, int leftIndex, int rightIndex) {
 		T[] arrayAux = (T[]) new Comparable[array.length];
 		mergeSort(array, arrayAux, leftIndex, rightIndex);
@@ -26,7 +58,7 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 		
 	}
 
-	private void intercalar(T[] array, T[] arrayAux, int inicio, int meio, int fim) {
+	/*private void intercalar(T[] array, T[] arrayAux, int inicio, int meio, int fim) {
 		for(int k = inicio; k <= fim; k++) {
 			arrayAux[k] = array[k];
 		}
@@ -46,4 +78,6 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 			}
 		}
 	}
+	*/
+
 }
