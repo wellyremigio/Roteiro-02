8349f3a3cb977402098c72576edb8817c406c84c
Welly Remígio <85763844+wellyremigio@users.noreply.github.com>
2023-04-16 20:06:06 -0300

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -12,45 +12,44 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		if(leftIndex < rightIndex) {
-			int middle = (leftIndex + rightIndex) / 2;
-			sort(array, leftIndex, middle);
-			sort(array, middle+1, rightIndex);
-			merge(array, leftIndex, middle, rightIndex);
+		mergeSort(array, leftIndex,rightIndex);
+	}
+	
+	private void mergeSort(T[] array, int inicio, int fim) {
+		if(inicio >= fim || array.length <= 0 || fim > array.length) {
+			return;
+		}else {
+			int meio = (inicio + fim) / 2;
+			mergeSort(array, inicio, meio);
+			mergeSort(array, meio+1, fim);
+			merge(array, inicio, meio, fim);
 		}
 	}
-
+	
 	private void merge(T[] array, int inicio, int meio, int fim) {
 		T[] arrayAux = (T[]) new Comparable[array.length];
 		for(int k = inicio; k <= fim; k++) {
 			arrayAux[k] = array[k];
 		}
-	
+		
 		int i = inicio;
 		int j = meio+1;
 		int k = inicio;
 		
 		while(i <= meio && j <= fim) {
-			if(arrayAux[i].compareTo(arrayAux[j]) < 0){
-				array[k] = arrayAux[i];
-				i++;
+			if(arrayAux[i].compareTo(arrayAux[j]) < 0 || arrayAux[i].compareTo(arrayAux[j]) == 0) {
+				array[k++] = arrayAux[i++];
 			}else {
-				array[k] = arrayAux[j];
-				j++;
+				array[k++] = arrayAux[j++];
 			}
-			k++;
-			
 		}
 		
-		while (i <= meio) {
-			array[k] = arrayAux[i];
-			i++;
-			k++;
+		while(i <= meio) {
+			array[k++] = arrayAux[i++];
 		}
-		while(j <= meio) {
-			array[k] = arrayAux[j];
-			j++;
-			k++;
+		
+		while(j <= fim) {
+			array[k++] = arrayAux[j++];
 		}
 	}
 }
